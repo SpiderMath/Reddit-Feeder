c850a1baafe563925ac659b2fdaa8e642c09c360
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-26 20:25:15 +0530

    Ugh, this is a mess

--- Source/extension.ts
+++ Source/extension.ts
@@ -1,26 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "reddit-feeder" is now active!');
+function wish(): string {
+	const currentTime = new Date().getHours();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('reddit-feeder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Reddit_Feeder!');
-	});
-
-	context.subscriptions.push(disposable);
+	if(currentTime > 5 && currentTime < 12) return "Good Morning";
+	else if(currentTime >= 12 && currentTime < 17) return "Good Afternoon";
+	else return "Good Evening";
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function activate() {
+	vscode.window.showInformationMessage(wish());
+}
